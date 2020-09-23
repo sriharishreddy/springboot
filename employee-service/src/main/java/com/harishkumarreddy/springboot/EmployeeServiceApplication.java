037f2b904f89db5b20de8aedceaf45d5d2e62ff9
ChiyetiHarishkumarRe <ChiyetiHarishkumarRe@DESKTOP-1V83DBU>
2020-09-23 21:55:46 +0530

    employee service

--- employee-service/src/main/java/com/harishkumarreddy/springboot/EmployeeServiceApplication.java
+++ employee-service/src/main/java/com/harishkumarreddy/springboot/EmployeeServiceApplication.java
@@ -2,10 +2,12 @@ package com.harishkumarreddy.springboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories
+@EnableEurekaClient
 public class EmployeeServiceApplication {
 
 	public static void main(String[] args) {
